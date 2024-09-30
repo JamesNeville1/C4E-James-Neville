21787c74d05913b3c80961eb93f80eb9e04c3095
Izzet <jamesliamneville@gmail.com>
2024-09-30 19:47:40 +0100

    Week 0 - Complete

---
+++ Source/C4E_Neville/Private/FirstActor.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FirstActor.h"
+
+#include "Components/ArrowComponent.h"
+
+
+// Sets default values
+AFirstActor::AFirstActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	_Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = _Root;
+
+	_Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	_Mesh -> SetupAttachment(_Root);
+
+	_Arrow = CreateDefaultSubobject<UArrowComponent>(TEXT("Arrow"));
+	_Arrow -> SetupAttachment(_Root);
+
+	_RotatingMovement = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("Rotating Movement"));
+}
---
+++ Source/C4E_Neville/Private/FirstActor.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GameFramework/RotatingMovementComponent.h"
+#include "FirstActor.generated.h"
+
+class UArrowComponent;
+
+UCLASS()
+class C4E_NEVILLE_API AFirstActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AFirstActor();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USceneComponent> _Root;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UStaticMeshComponent> _Mesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UArrowComponent> _Arrow;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<URotatingMovementComponent> _RotatingMovement;
+};
